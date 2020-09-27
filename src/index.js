a186a704e4be6e6b1536bf10050e5a5fb845fd0f
claudia223 <claudia2001d@gmail.com>
2020-09-27 18:24:47 +0100

    Add nextMoves and applyMove functions to update game state

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-var ld = require("lodash");
-var ArrayKeyedMap = require("array-keyed-map");
+var L = require('lodash');
+var ArrayKeyedMap = require('array-keyed-map');
 
 // data Suits = Diamond | Clubs | Hearts | Spades
 var SUITS = new Map([
-    ["DIAMONDS", "A"],
-    ["CLUBS", "B"],
+    ['DIAMONDS', 'A'],
+    ['CLUBS', 'B'],
     // HEARTS: "H",
     // SPADES: "S"
 ]);
 
-const N_CARDS = 5;
+var N_CARDS = 5;
 
 // data Card = Yes | No | Turned
 var CARD_STATE = Object.freeze({
     YES: 1,
     NO: 0,
     TURNED: -1,
-})
+});
 
 function shuffleCards(players) {
     var cards = [];
@@ -27,40 +27,117 @@ function shuffleCards(players) {
         }
     }
 
-    const cards_per_player = (cards.length / players);
-    cards =  ld.shuffle(cards.slice(cards.length % players));
+    const cards_per_player = cards.length / players;
+    cards = L.shuffle(cards.slice(cards.length % players));
 
-    return ld.range(3)
-        .map(i => cards.slice(i * cards_per_player, (i + 1) * cards_per_player));
-};
+    return L.range(3).map((i) =>
+        cards.slice(i * cards_per_player, (i + 1) * cards_per_player)
+    );
+}
+
+function nextPlayer(currentPlayer, totalPlayers) {
+    return (currentPlayer % totalPlayers) + 1;
+}
 
 function makeState(playerCards) {
-    const N = playerCards.length;
-    var heads = new Map();
+    var limits = new Map();
     var hands = new ArrayKeyedMap();
+    var totalPlayers = playerCards.length;
     var firstCard = [SUITS.values().next().value, Math.ceil(N_CARDS / 2)];
+    var [firstSuit, firstNum] = firstCard;
 
     for (let suit of SUITS.values()) {
-        var head = suit === firstCard[0] ? [firstCard[1],firstCard[1]] : null;
-        heads.set(suit,head);
+        let limit = { end: [0, N_CARDS] };
+
+        if (suit === firstSuit) {
+            limit.start = [firstNum - 1, firstNum + 1];
+        } else {
+            limit.start = [firstNum, firstNum];
+        }
+
+        limits.set(suit, limit);
     }
 
-    for(let [i, cards] of playerCards.entries()){
-        for(let card of cards){
+    for (let [i, cards] of playerCards.entries()) {
+        for (let card of cards) {
             hands.set(card, i + 1);
         }
     }
 
-    // Number of first player as an index [0, N - 1]
-    var firstPlayer = hands.get(firstCard) - 1;
-    // Number of next player as an index [1, N]
-    var currentPlayer = ((firstPlayer + 1) % N) + 1;
-
+    var currentPlayer = nextPlayer(hands.get(firstCard), totalPlayers);
     hands.set(firstCard, 0);
 
-    return {heads, hands, currentPlayer};
+    return { limits, hands, currentPlayer, totalPlayers };
 }
-
 
 var cards = shuffleCards(3);
 var state = makeState(cards);
+
+function playerCards(state, player) {
+    const cards = [];
+
+    for (let [card, cardPlayer] of state.hands.entries()) {
+        if (cardPlayer === player) {
+            cards.push(card);
+        }
+    }
+
+    return cards;
+}
+
+function nextMoves(state) {
+    var { limits, hands, currentPlayer } = state;
+    cards = [];
+
+    for (let [suit, { start }] of limits.entries()) {
+        for (let num of start) {
+            let card = [suit, num];
+            if (!isNaN(num) && hands.get(card) === currentPlayer) {
+                cards.push(card);
+            }
+        }
+    }
+
+    if (L.isEmpty(cards)) {
+        return { action: 'turn', cards: playerCards(state, currentPlayer) };
+    } else {
+        return { action: 'play', cards };
+    }
+}
+
+function applyMove(state, move) {
+    let [suit, num] = move.card;
+    let { start, end } = state.limits.get(suit);
+
+    switch (move.action) {
+        case 'play':
+            let [min, max] = start;
+
+            state.hands.set(move.card, 0);
+
+            if (num === min) {
+                start[0] = min === end[0] ? NaN : min - 1;
+            } else if (num === max) {
+                start[1] = max === end[1] ? NaN : max + 1;
+            } else if (min === max) {
+                start[0] = min === end[0] ? NaN : min - 1;
+                start[1] = max === end[1] ? NaN : max + 1;
+            }
+
+            break;
+        case 'turn':
+            if (num < start[0]) {
+                end[0] = num;
+            } else if (num > start[1]) {
+                end[1] = num;
+            }
+
+            state.hands.set(move.card, -state.currentPlayer);
+
+            break;
+    }
+
+    state.currentPlayer = nextPlayer(state.currentPlayer, state.totalPlayers);
+
+    return state;
+}
